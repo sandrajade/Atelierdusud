5472d0d529b8cdb11e0a8e6d93d8fd19ca96650a
vinvui <admin@vinvui.com>
2023-10-10 11:13:23 +0200

    Manage some UX/UI with Sandra

--- resources/views/atelierdusud/artists/show.blade.php
+++ resources/views/atelierdusud/artists/show.blade.php
@@ -1,15 +1,22 @@
 <x-app-layout>
-    <x-slot>
-    <h2>Artistes</h2>
-    <a href="{{ route('artists.index')}}">Retour aux artistes</a>
-    </x-slot>
+
+    <div class="bg-white sticky top-0 shadow-lg">
+        <div class="flex items-center justify-between px-8 py-2">
+            <h2 class="font-semibold font-amaranth text-xl text-gray-800 leading-tight">
+                👩‍🎨 Artistes > {{ $artist->name }}
+            </h2>
+            <a href="{{ route('artists.index') }}" class="font-amaranth border-2 border-yellow-300 rounded-md hover:bg-yellow-300 py-2 px-3 transition-all duration-300">
+                ⬅️ Retour aux artistes
+            </a>
+        </div>
+    </div>
 
     <div>
         <div>
             <div>
                 <div>
                     <label for="name">Name</label>
-                    {{ $artiste->name }}
+                    {{ $artist->name }}
                 </div>
             </div>
             <div>
@@ -28,24 +35,20 @@
                     <div>
                         <ul>
                             <li>Collection {{ $collection->title}}</li>
+                            <label for="name">Oeuvres</label>
+                            @foreach ($collection->works as $work)
+                            <div>
+                                <ul>
+                                    <li>{{ $work->title }}</li>
+                                    <li>{{ $work->url }}</li>
+                                </ul>
+                            </div>
+                            @endforeach
                         </ul>
                     </div>
-
-                </div>
-                <div>
-                    <label for="name">Oeuvres</label>
-                    @foreach ($collection->works as $work)
-                    <div>
-                        <ul>
-                            <li>{{ $work->title }}</li>
-                            <li>{{ $work->url }}</li>
-
-                        </ul>
-                    </div>
-
                     @endforeach
+
                 </div>
-                @endforeach
             </div>
         </div>
     </div>
